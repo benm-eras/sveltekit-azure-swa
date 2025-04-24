33c8a19ac0942b572c77fd9e537a397276606c11
Ben Miller <BenM@eras.co.uk>
2025-04-24 09:42:08 +0100

    ci/cd test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    This is a test from Ben just seeing if the CI/CD pipeline works at 9:41.
+</p>
