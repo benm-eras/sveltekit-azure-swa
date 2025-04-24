41650330ea79bf3db5570cdd41c0a5537865dc4e
Ben Miller <BenM@eras.co.uk>
2025-04-24 09:58:45 +0100

    added note to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>
-    This is a test from Ben just seeing if the CI/CD pipeline works at 9:41.
-</p>
+
+<ol>
+    <li>
+        This is a test from Ben just seeing if the CI/CD pipeline works at 9:41.
+    </li>
+    <li>
+        It is now 9:57 and this was created on the dev branch.
+    </li>
+</ol>
